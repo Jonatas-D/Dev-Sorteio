0e0ae93a926570b2808ae428773c98439f721a2b
@Isa102088 <dourado2288@gmail.com>
2024-03-16 16:03:10 -0300

    Primeiro commint - projeto Dev Sorteio

---
+++ script.js
@@ -0,0 +1,14 @@
+function generateNumber() {
+
+      const min = Math.ceil(document.querySelector(".input-min").value)
+      const max = Math.floor(document.querySelector(".input-max").value)
+
+
+      if (min >= max) {
+            alert("O Valor Minimo tem quer ser MENOR que o Valor Máximo")
+      } else {
+            const result = Math.floor(Math.random() * (max - min + 1)) + min;
+            alert(result)
+      }
+
+}
